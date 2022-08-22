0de2eb959055c1644a7c78807bba007d2f613eb4
Tehaam Hashmi <htehaam@outlook.com>
2022-08-22 08:17:50 +0530

    Added Login and profile endpoints

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ const bcrypt = require("bcryptjs");
 const fs = require("fs");
 const path = require("path");
 const fetchUser = require("../middleware/fetchUser");
+const  { body, validationResult } = require("express-validator");
 
 // DEFINING DIRECTORY TO SAVE THE UPLOADED IMAGE
 
@@ -92,31 +93,56 @@ router.post("/add", uploadImage, async (req, res) => {
 
 // LOGIN
 
-router.post('/login', async(req, res) => {
-  let success = false
-  try {
-    const foundUser = await Card.findOne({email: req.body.email})
-    if(!foundUser) {
-      res.status(404).json({success, error: "User does not exist, please register"})
-    } else {
-      const data = {
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email address").isEmail(),
+    body("password", "Password must be entered").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    let success = false
+    // If the teacher fails validation
+    if (!errors.isEmpty()) {
+      console.log(errors.array()[0].msg);
+      return res.status(400).json({success, errors: errors.array() });
+    }
+
+    try {
+      // Checking if the user exists in database
+      let foundUser = await Card.findOne({ email: req.body.email }).select(['_id', 'email', 'password'])
+      console.log(foundUser)
+
+      if (!foundUser) {
+        return res.status(404).json({success, error: "Credentials do not match" });
+      }
+
+      const passwordCompare = await bcrypt.compare(
+        req.body.password,
+        foundUser.password
+      );
+
+      if (!passwordCompare) {
+        return res.status(400).json({success, data: "Credentials do not match" });
+      }
+
+      //Generating an auth token
+      const payload = {
         user: {
           id: foundUser._id,
         },
       };
 
-      const jwtData = jwt.sign(data, "test_secret");
+      const jwtData = jwt.sign(payload, "test_secret");
       console.log(jwtData);
-
       success = true
-      res.status(200).json({success, token: jwtData})
-
+      res.json({success, token: jwtData });
+    } catch (err) {
+      console.log(err);
+      res.send(err);
     }
-
-  } catch(error) {
-    res.status(500).send(error.message)
   }
-})
+);
 
 
 // GETTING ALL USERS - AUTHENTICATION REQUIRED
@@ -141,5 +167,32 @@ router.get('/all', fetchUser, async (req, res) => {
         res.status(500).json({success, error: err.message})
     }
 })
+
+
+// GET USER PROFILE
+
+
+router.get('/profile', fetchUser, async (req, res) => {
+  let success = false
+  try {
+    const userId = req.user.id
+    const userExists = await Card.findOne({_id: userId})
+    if(!userExists) {
+      res.status(401).json({success})
+    }
+      const ourUser = await Card.find({_id: userId }).select([
+        'name',
+        'image',
+        'email'
+      ])
+      if(ourUser) {
+          success = true
+          res.status(200).json({success, ourUser})
+      }
+  } catch(err) {
+      res.status(500).json({success, error: err.message})
+  }
+})
+
 
 module.exports = router;
